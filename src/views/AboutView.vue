b34cd8d126e83df65560388de9559cce01834510
Axel Goris <goris@kth.se>
2022-05-15 19:14:59 +0200

    Feat: Working deployed website

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Une page d'informations diverses et variées.</h1>
+    <h2>
+      Réalisée par :
+      <a href="https://axelgoris99.github.io/" target="_blank"> Axel Goris</a>
+    </h2>
   </div>
 </template>
