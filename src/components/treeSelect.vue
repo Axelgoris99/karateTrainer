a0ec53c1c881be1de6eada96693edfcd58e0261f
Axel Goris <goris@kth.se>
2022-05-15 20:30:09 +0200

    Feat: Home page done

---
+++ src/components/treeSelect.vue
@@ -0,0 +1,117 @@
+<template>
+  <n-space vertical>
+    <n-tree-select
+      multiple
+      cascade
+      checkable
+      :check-strategy="checkStrategy"
+      :options="options"
+      @update:value="handleUpdateValue"
+    />
+  </n-space>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {
+      checkStrategy: ref("parent"),
+      options: [
+        {
+          label: "Blocage",
+          key: "Blocage",
+          children: [
+            {
+              label: "Drive My Car",
+              key: "Drive My Car",
+              disabled: true,
+            },
+            {
+              label: "Norwegian Wood",
+              key: "Norwegian Wood",
+            },
+            {
+              label: "You Won't See",
+              key: "You Won't See",
+              disabled: true,
+            },
+            {
+              label: "Nowhere Man",
+              key: "Nowhere Man",
+            },
+            {
+              label: "Think For Yourself",
+              key: "Think For Yourself",
+            },
+          ],
+        },
+        {
+          label: "Attaque",
+          key: "Attaque",
+          children: [
+            {
+              label: "Pied",
+              key: "Pied",
+              children: [
+                {
+                  label: "Dig A Pony",
+                  key: "Dig A Pony",
+                },
+              ],
+            },
+
+            {
+              label: "Poing",
+              key: "Poing",
+              children: [
+                {
+                  label: "Dig It",
+                  key: "Dig It",
+                },
+                {
+                  label: "go",
+                  key: "go",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          label: "Mouvement",
+          key: "Mouvement",
+          children: [
+            {
+              label: "Two Of Us",
+              key: "Two Of Us",
+            },
+            {
+              label: "Dig A Pony",
+              key: "Dig A Pony",
+            },
+          ],
+        },
+        {
+          label: "Position",
+          key: "Position",
+          children: [
+            {
+              label: "Two Of Us",
+              key: "Two Of Us",
+            },
+            {
+              label: "Dig A Pony",
+              key: "Dig A Pony",
+            },
+          ],
+        },
+      ],
+      // handleUpdateValue(value, option) {
+      // console.log(value, option);
+      // },
+    };
+  },
+});
+</script>
+<style lang="scss"></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,79 @@
 <template>
   <div class="home">
-    <img alt="ISK logo" src="../assets/logo.png" />
+    <n-space vertical>
+      <treeSelect></treeSelect>
+      <treeSelect></treeSelect>
+      <treeSelect></treeSelect>
+      <treeSelect></treeSelect>
+      <treeSelect></treeSelect>
+
+      <n-space horizontal justify="center">
+        <n-button :loading="loading" icon-placement="left" @click="handleClick">
+          Générer
+        </n-button>
+        <n-button strong type="warning">Reset !</n-button>
+      </n-space>
+      <n-card v-if="affiche" title="Techniques" style="margin-bottom: 16px">
+        <n-space horizontal justify="center">
+          <n-tabs type="line" animated>
+            <n-tab-pane name="Mawashi" tab="Mawashi">
+              <n-space vertical>
+                <span>Mawashi : Une petite description / Conseil</span>
+                <n-image :src="require('@/assets/mawashi.png')" />
+              </n-space>
+            </n-tab-pane>
+            <n-tab-pane name="Gyaku" tab="Gyaku">
+              <n-space vertical>
+                <span>
+                  Gyaku : Une petite description / Conseil ou encore bien plus
+                  si affinités !</span
+                >
+                <n-image :src="require('@/assets/gyaku.png')" />
+              </n-space>
+            </n-tab-pane>
+            <n-tab-pane name="GedanBarai" tab="Gedan Barai">
+              <n-space vertical>
+                <span>Gedan Barai : Une petite description / Conseil</span>
+                <n-image :src="require('@/assets/gedan.jpg')" />
+              </n-space>
+            </n-tab-pane>
+          </n-tabs>
+        </n-space>
+      </n-card>
+    </n-space>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import treeSelect from "../components/treeSelect.vue";
 
 export default {
   name: "HomeView",
-  components: {},
+  components: { treeSelect },
+  data() {
+    return {
+      loading: false,
+      affiche: false,
+    };
+  },
+  methods: {
+    handleClick() {
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = false;
+        this.affiche = true;
+      }, 2000);
+    },
+  },
 };
 </script>
+<style lang="scss">
+.home {
+  margin-left: 5vw;
+  margin-right: 5vw;
+}
+.card-tabs .n-tabs-nav--bar-type {
+  padding-left: 4px;
+}
+</style>
