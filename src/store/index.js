ded7423986a0c4d069e482befae90e87ac8d1762
Axel Goris <axelgoris99@gmail.com>
2022-05-20 17:31:41 +0200

    Feat: Filled store with datas for techniques

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,33 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    techniques: {},
+    selected: [],
+  },
+  getters: {
+    techniques(state) {
+      return state.techniques;
+    },
+    selected(state) {
+      return state.selected;
+    },
+  },
+  mutations: {
+    LOAD_TECHNIQUES(state, tech) {
+      state.techniques = tech;
+    },
+    ADD_SELECTED(state, s) {
+      state.selected.push(s);
+    },
+  },
+  actions: {
+    setTechniques({ commit }, tech) {
+      commit("LOAD_TECHNIQUES", tech);
+    },
+    setSelected({ commit }, s) {
+      commit("ADD_SELECTED", s);
+    },
+  },
   modules: {},
 });
