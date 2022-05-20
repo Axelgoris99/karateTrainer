0d061f312532dca710ec281f1a306fbe1036af73
Axel Goris <axelgoris99@gmail.com>
2022-05-20 17:32:50 +0200

    Feat: Changed the view a bit, added v-if, bootstrapped options loading

--- src/components/treeSelect.vue
+++ src/components/treeSelect.vue
@@ -4,7 +4,9 @@
       multiple
       cascade
       checkable
-      :check-strategy="checkStrategy"
+      placeholder="Sélectionner les techniques désirées"
+      clearable
+      check-strategy="parent"
       :options="options"
       @update:value="handleUpdateValue"
     />
@@ -12,106 +14,16 @@
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
-
-export default defineComponent({
-  setup() {
-    return {
-      checkStrategy: ref("parent"),
-      options: [
-        {
-          label: "Blocage",
-          key: "Blocage",
-          children: [
-            {
-              label: "Drive My Car",
-              key: "Drive My Car",
-              disabled: true,
-            },
-            {
-              label: "Norwegian Wood",
-              key: "Norwegian Wood",
-            },
-            {
-              label: "You Won't See",
-              key: "You Won't See",
-              disabled: true,
-            },
-            {
-              label: "Nowhere Man",
-              key: "Nowhere Man",
-            },
-            {
-              label: "Think For Yourself",
-              key: "Think For Yourself",
-            },
-          ],
-        },
-        {
-          label: "Attaque",
-          key: "Attaque",
-          children: [
-            {
-              label: "Pied",
-              key: "Pied",
-              children: [
-                {
-                  label: "Dig A Pony",
-                  key: "Dig A Pony",
-                },
-              ],
-            },
-
-            {
-              label: "Poing",
-              key: "Poing",
-              children: [
-                {
-                  label: "Dig It",
-                  key: "Dig It",
-                },
-                {
-                  label: "go",
-                  key: "go",
-                },
-              ],
-            },
-          ],
-        },
-        {
-          label: "Mouvement",
-          key: "Mouvement",
-          children: [
-            {
-              label: "Two Of Us",
-              key: "Two Of Us",
-            },
-            {
-              label: "Dig A Pony",
-              key: "Dig A Pony",
-            },
-          ],
-        },
-        {
-          label: "Position",
-          key: "Position",
-          children: [
-            {
-              label: "Two Of Us",
-              key: "Two Of Us",
-            },
-            {
-              label: "Dig A Pony",
-              key: "Dig A Pony",
-            },
-          ],
-        },
-      ],
-      // handleUpdateValue(value, option) {
-      // console.log(value, option);
-      // },
-    };
+export default {
+  name: "techniqueComp",
+  props: {
+    options: Object,
   },
-});
+  methods: {
+    handleUpdateValue(value) {
+      console.log(value);
+    },
+  },
+};
 </script>
 <style lang="scss"></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,53 +1,69 @@
 <template>
   <div class="home">
     <n-space vertical>
-      <treeSelect></treeSelect>
-      <treeSelect></treeSelect>
-      <treeSelect></treeSelect>
-      <treeSelect></treeSelect>
-      <treeSelect></treeSelect>
-
+      <h2>Nombre de Techniques</h2>
+      <n-input-number
+        v-model:value="count"
+        clearable
+        :max="8"
+        :min="0"
+        placeholder="Nombre de Techniques"
+      />
+      <h2>Choix de techniques spécifiques</h2>
+      <p>
+        Si aucun choix n'est fait pour une case alors une technique aléatoire
+        sera sélectionné
+      </p>
+      <div v-for="n in count" :key="n">
+        <span>Technique n°{{ n }}</span>
+        <treeSelect :options="options"></treeSelect>
+      </div>
+      <h2>Génération ou Reset</h2>
       <n-space horizontal justify="center">
         <n-button :loading="loading" icon-placement="left" @click="handleClick">
           Générer
         </n-button>
-        <n-button strong type="warning">Reset !</n-button>
+        <n-button strong type="warning" @click="reset">Reset !</n-button>
       </n-space>
-      <n-card v-if="affiche" title="Techniques" style="margin-bottom: 16px">
-        <n-space horizontal justify="center">
-          <n-tabs type="line" animated>
-            <n-tab-pane name="Mawashi" tab="Mawashi">
-              <n-space vertical>
-                <span>Mawashi : Une petite description / Conseil</span>
-                <n-image :src="require('@/assets/mawashi.png')" />
-              </n-space>
-            </n-tab-pane>
-            <n-tab-pane name="Gyaku" tab="Gyaku">
-              <n-space vertical>
-                <span>
-                  Gyaku : Une petite description / Conseil ou encore bien plus
-                  si affinités !</span
-                >
-                <n-image :src="require('@/assets/gyaku.png')" />
-              </n-space>
-            </n-tab-pane>
-            <n-tab-pane name="GedanBarai" tab="Gedan Barai">
-              <n-space vertical>
-                <span>Gedan Barai : Une petite description / Conseil</span>
-                <n-image :src="require('@/assets/gedan.jpg')" />
-              </n-space>
-            </n-tab-pane>
-          </n-tabs>
-        </n-space>
-      </n-card>
+      <div v-if="affiche">
+        <h2>Ecoute</h2>
+        <h2>Aide pour les techniques</h2>
+        <n-card title="Techniques" style="margin-bottom: 16px">
+          <n-space horizontal justify="center">
+            <n-tabs type="line" animated>
+              <n-tab-pane name="Mawashi" tab="Mawashi">
+                <n-space vertical>
+                  <span>Mawashi : Une petite description / Conseil</span>
+                  <n-image :src="require('@/assets/mawashi.png')" />
+                </n-space>
+              </n-tab-pane>
+              <n-tab-pane name="Gyaku" tab="Gyaku">
+                <n-space vertical>
+                  <span>
+                    Gyaku : Une petite description / Conseil ou encore bien plus
+                    si affinités !</span
+                  >
+                  <n-image :src="require('@/assets/gyaku.png')" />
+                </n-space>
+              </n-tab-pane>
+              <n-tab-pane name="GedanBarai" tab="Gedan Barai">
+                <n-space vertical>
+                  <span>Gedan Barai : Une petite description / Conseil</span>
+                  <n-image :src="require('@/assets/gedan.jpg')" />
+                </n-space>
+              </n-tab-pane>
+            </n-tabs>
+          </n-space>
+        </n-card>
+      </div>
     </n-space>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import treeSelect from "../components/treeSelect.vue";
-
+import { fetchAllData } from "../firebaseModel.js";
+import { mapGetters } from "vuex";
 export default {
   name: "HomeView",
   components: { treeSelect },
@@ -55,7 +71,40 @@ export default {
     return {
       loading: false,
       affiche: false,
+      count: 3,
     };
+  },
+  computed: {
+    ...mapGetters({
+      techniques: "techniques",
+    }),
+    options() {
+      var types = [];
+      for (let [key, value] of Object.entries(this.techniques)) {
+        var children = [];
+        for (let [key1, value1] of Object.entries(value)) {
+          if (key == "attaques") {
+            var treeFeet = [];
+            for (let [key2, value2] of Object.entries(value1)) {
+              treeFeet.push({ label: value2.name, key: key2 });
+            }
+            children.push({
+              label: key1.toUpperCase(),
+              key: key1,
+              children: treeFeet,
+            });
+          } else {
+            children.push({ label: value1.name, key: key1 });
+          }
+        }
+        var tree = { label: key.toUpperCase(), key: key, children: children };
+        types.push(tree);
+      }
+      return types;
+    },
+  },
+  created() {
+    fetchAllData();
   },
   methods: {
     handleClick() {
@@ -64,6 +113,9 @@ export default {
         this.loading = false;
         this.affiche = true;
       }, 2000);
+    },
+    reset() {
+      console.log("reset");
     },
   },
 };
